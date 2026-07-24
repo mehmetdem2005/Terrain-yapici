640231b1c3f7354cd8d9401534fe896e01ff0765
mehmetdem2005 <mehmetdem2005@gmail.com>
2026-07-24 23:35:03 +0300

    Integrate mutually exclusive sculpt and paint editor sessions

--- addons/island_terrain/island_terrain_plugin.gd
+++ addons/island_terrain/island_terrain_plugin.gd
@@ -2,34 +2,58 @@
 extends EditorPlugin
 
 const TerrainNode = preload("res://addons/island_terrain/island_terrain_3d.gd")
-const Toolbar = preload("res://addons/island_terrain/editor/terrain_sculpt_toolbar.gd")
+const SculptToolbar = preload("res://addons/island_terrain/editor/terrain_sculpt_toolbar.gd")
 const SculptSession = preload("res://addons/island_terrain/editor/terrain_sculpt_session.gd")
+const PaintToolbar = preload("res://addons/island_terrain/editor/terrain_paint_toolbar.gd")
+const PaintSession = preload("res://addons/island_terrain/editor/terrain_paint_session.gd")
 
-var _toolbar: Toolbar
-var _session: SculptSession
+var _sculpt_toolbar: SculptToolbar
+var _sculpt_session: SculptSession
+var _paint_toolbar: PaintToolbar
+var _paint_session: PaintSession
 
 
 func _enter_tree() -> void:
 	add_custom_type("IslandTerrain3D", "Node3D", TerrainNode, null)
-	_toolbar = Toolbar.new()
-	_toolbar.visible = false
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, _toolbar)
-	_session = SculptSession.new()
-	_session.configure(_toolbar, get_undo_redo())
-	_toolbar.undo_requested.connect(_session.undo)
-	_toolbar.redo_requested.connect(_session.redo)
+
+	_sculpt_toolbar = SculptToolbar.new()
+	_sculpt_toolbar.visible = false
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, _sculpt_toolbar)
+	_sculpt_session = SculptSession.new()
+	_sculpt_session.configure(_sculpt_toolbar, get_undo_redo())
+	_sculpt_toolbar.undo_requested.connect(_sculpt_session.undo)
+	_sculpt_toolbar.redo_requested.connect(_sculpt_session.redo)
+	_sculpt_toolbar.sculpt_mode_changed.connect(_on_sculpt_mode_changed)
+
+	_paint_toolbar = PaintToolbar.new()
+	_paint_toolbar.visible = false
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, _paint_toolbar)
+	_paint_session = PaintSession.new()
+	_paint_session.configure(_paint_toolbar, get_undo_redo())
+	_paint_toolbar.undo_requested.connect(_paint_session.undo)
+	_paint_toolbar.redo_requested.connect(_paint_session.redo)
+	_paint_toolbar.paint_mode_changed.connect(_on_paint_mode_changed)
 	call_deferred("_validate_project_renderer")
 
 
 func _exit_tree() -> void:
-	if _session != null:
-		_session.finalize_stroke()
-		_session.clear_terrain()
-	_session = null
-	if is_instance_valid(_toolbar):
-		remove_control_from_docks(_toolbar)
-		_toolbar.queue_free()
-	_toolbar = null
+	if _paint_session != null:
+		_paint_session.finalize_stroke()
+		_paint_session.clear_terrain()
+	_paint_session = null
+	if is_instance_valid(_paint_toolbar):
+		remove_control_from_docks(_paint_toolbar)
+		_paint_toolbar.queue_free()
+	_paint_toolbar = null
+
+	if _sculpt_session != null:
+		_sculpt_session.finalize_stroke()
+		_sculpt_session.clear_terrain()
+	_sculpt_session = null
+	if is_instance_valid(_sculpt_toolbar):
+		remove_control_from_docks(_sculpt_toolbar)
+		_sculpt_toolbar.queue_free()
+	_sculpt_toolbar = null
 	remove_custom_type("IslandTerrain3D")
 
 
@@ -38,21 +62,47 @@ func _handles(object: Object) -> bool:
 
 
 func _edit(object: Object) -> void:
-	if _session != null:
-		_session.set_terrain(object as TerrainNode)
+	var terrain := object as TerrainNode
+	if _sculpt_session != null:
+		_sculpt_session.set_terrain(terrain)
+	if _paint_session != null:
+		_paint_session.set_terrain(terrain)
 
 
 func _make_visible(visible: bool) -> void:
-	if is_instance_valid(_toolbar):
-		_toolbar.visible = visible
-	if not visible and _session != null:
-		_session.clear_terrain()
+	if is_instance_valid(_sculpt_toolbar):
+		_sculpt_toolbar.visible = visible
+	if is_instance_valid(_paint_toolbar):
+		_paint_toolbar.visible = visible
+	if not visible:
+		if _sculpt_session != null:
+			_sculpt_session.clear_terrain()
+		if _paint_session != null:
+			_paint_session.clear_terrain()
 
 
 func _forward_3d_gui_input(camera: Camera3D, event: InputEvent) -> int:
-	return _session.route_input(camera, event) \
-		if _session != null \
+	if _paint_session != null:
+		var paint_result: int = _paint_session.route_input(camera, event)
+		if paint_result == EditorPlugin.AFTER_GUI_INPUT_STOP:
+			return paint_result
+	return _sculpt_session.route_input(camera, event) \
+		if _sculpt_session != null \
 		else EditorPlugin.AFTER_GUI_INPUT_PASS
+
+
+func _on_sculpt_mode_changed(enabled: bool) -> void:
+	if enabled and is_instance_valid(_paint_toolbar):
+		_paint_toolbar.disable_paint()
+	if enabled and _paint_session != null:
+		_paint_session.finalize_stroke()
+
+
+func _on_paint_mode_changed(enabled: bool) -> void:
+	if enabled and is_instance_valid(_sculpt_toolbar):
+		_sculpt_toolbar.disable_sculpt()
+	if enabled and _sculpt_session != null:
+		_sculpt_session.finalize_stroke()
 
 
 func _validate_project_renderer() -> void:

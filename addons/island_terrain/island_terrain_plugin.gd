e6221aaa39e733136fec9735bbe659a717cf02b0
mehmetdem2005 <mehmetdem2005@gmail.com>
2026-07-25 05:55:01 +0300

    Expose sculpt and paint tools in a phone-accessible bottom panel

--- addons/island_terrain/island_terrain_plugin.gd
+++ addons/island_terrain/island_terrain_plugin.gd
@@ -2,11 +2,13 @@
 extends EditorPlugin
 
 const TerrainNode = preload("res://addons/island_terrain/island_terrain_3d.gd")
+const ToolsPanel = preload("res://addons/island_terrain/editor/terrain_tools_panel.gd")
 const SculptToolbar = preload("res://addons/island_terrain/editor/terrain_sculpt_toolbar.gd")
 const SculptSession = preload("res://addons/island_terrain/editor/terrain_sculpt_session.gd")
 const PaintToolbar = preload("res://addons/island_terrain/editor/terrain_paint_toolbar.gd")
 const PaintSession = preload("res://addons/island_terrain/editor/terrain_paint_session.gd")
 
+var _tools_panel: ToolsPanel
 var _sculpt_toolbar: SculptToolbar
 var _sculpt_session: SculptSession
 var _paint_toolbar: PaintToolbar
@@ -16,18 +18,18 @@ var _paint_session: PaintSession
 func _enter_tree() -> void:
 	add_custom_type("IslandTerrain3D", "Node3D", TerrainNode, null)
 
+	_tools_panel = ToolsPanel.new()
 	_sculpt_toolbar = SculptToolbar.new()
-	_sculpt_toolbar.visible = false
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, _sculpt_toolbar)
+	_paint_toolbar = PaintToolbar.new()
+	_tools_panel.configure(_sculpt_toolbar, _paint_toolbar)
+	add_control_to_bottom_panel(_tools_panel, "IslandTerrain")
+
 	_sculpt_session = SculptSession.new()
 	_sculpt_session.configure(_sculpt_toolbar, get_undo_redo())
 	_sculpt_toolbar.undo_requested.connect(_sculpt_session.undo)
 	_sculpt_toolbar.redo_requested.connect(_sculpt_session.redo)
 	_sculpt_toolbar.sculpt_mode_changed.connect(_on_sculpt_mode_changed)
 
-	_paint_toolbar = PaintToolbar.new()
-	_paint_toolbar.visible = false
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, _paint_toolbar)
 	_paint_session = PaintSession.new()
 	_paint_session.configure(_paint_toolbar, get_undo_redo())
 	_paint_toolbar.undo_requested.connect(_paint_session.undo)
@@ -41,19 +43,18 @@ func _exit_tree() -> void:
 		_paint_session.finalize_stroke()
 		_paint_session.clear_terrain()
 	_paint_session = null
-	if is_instance_valid(_paint_toolbar):
-		remove_control_from_docks(_paint_toolbar)
-		_paint_toolbar.queue_free()
-	_paint_toolbar = null
 
 	if _sculpt_session != null:
 		_sculpt_session.finalize_stroke()
 		_sculpt_session.clear_terrain()
 	_sculpt_session = null
-	if is_instance_valid(_sculpt_toolbar):
-		remove_control_from_docks(_sculpt_toolbar)
-		_sculpt_toolbar.queue_free()
+
+	if is_instance_valid(_tools_panel):
+		remove_control_from_bottom_panel(_tools_panel)
+		_tools_panel.queue_free()
+	_tools_panel = null
 	_sculpt_toolbar = null
+	_paint_toolbar = null
 	remove_custom_type("IslandTerrain3D")
 
 
@@ -67,13 +68,13 @@ func _edit(object: Object) -> void:
 		_sculpt_session.set_terrain(terrain)
 	if _paint_session != null:
 		_paint_session.set_terrain(terrain)
+	if terrain != null and is_instance_valid(_tools_panel):
+		make_bottom_panel_item_visible(_tools_panel)
 
 
 func _make_visible(visible: bool) -> void:
-	if is_instance_valid(_sculpt_toolbar):
-		_sculpt_toolbar.visible = visible
-	if is_instance_valid(_paint_toolbar):
-		_paint_toolbar.visible = visible
+	if visible and is_instance_valid(_tools_panel):
+		make_bottom_panel_item_visible(_tools_panel)
 	if not visible:
 		if _sculpt_session != null:
 			_sculpt_session.clear_terrain()
@@ -96,6 +97,8 @@ func _on_sculpt_mode_changed(enabled: bool) -> void:
 		_paint_toolbar.disable_paint()
 	if enabled and _paint_session != null:
 		_paint_session.finalize_stroke()
+	if enabled and is_instance_valid(_tools_panel):
+		_tools_panel.select_sculpt_tab()
 
 
 func _on_paint_mode_changed(enabled: bool) -> void:
@@ -103,6 +106,8 @@ func _on_paint_mode_changed(enabled: bool) -> void:
 		_sculpt_toolbar.disable_sculpt()
 	if enabled and _sculpt_session != null:
 		_sculpt_session.finalize_stroke()
+	if enabled and is_instance_valid(_tools_panel):
+		_tools_panel.select_paint_tab()
 
 
 func _validate_project_renderer() -> void:
